1b182151e473a0f2d719b3b15a5e605114b6a807
Zhannur <jannuraidynuly@gmail.com>
2022-04-12 22:17:32 +0500

    connect Mongo and model db

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express";
+import mongoose from "mongoose";
+import Post from "./Post.js";
 
-const PORT = 5000;
+const DB_URL = `mongodb+srv://police:Police12@cluster0.nnice.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+const PORT = 5001;
 const app = express();
 app.use(express.json())
 
@@ -9,9 +12,23 @@ app.get('/', (req, res) => {
     res.status(200).json('Server job')
 });
 
-app.post('/', (req, res) => {
-    console.log(req.body);
-    res.status(200).json('server job')
+app.post('/', async (req, res) => {
+    try {
+        const { author, title, content, picture } = req.body
+        const post = await Post.create({author, title, content, picture})
+        res.json(post)
+    } catch (e) {
+        res.status(500).json(e)
+    }
 })
 
-app.listen(PORT, () => console.log('SERVER STARTED ON PORT ' + PORT))
+async function startApp(){
+    try {
+        await mongoose.connect(DB_URL)
+        app.listen(PORT, () => console.log('SERVER STARTED ON PORT ' + PORT))
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+startApp()
