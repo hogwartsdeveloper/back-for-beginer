b9d5cc28cd341047b04a61a368418e89f905a00c
Zhannur <jannuraidynuly@gmail.com>
2022-04-13 20:59:52 +0500

    fileService

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import fileUpload from "express-fileupload";
 import router from "./router.js";
 
 const DB_URL = `mongodb+srv://police:Police12@cluster0.nnice.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
@@ -7,6 +8,8 @@ const PORT = 5001;
 const app = express();
 
 app.use(express.json())
+app.use(express.static('static'))
+app.use(fileUpload({}))
 app.use('/api', router)
 
 async function startApp(){
