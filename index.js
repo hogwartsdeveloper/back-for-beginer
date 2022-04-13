e11cc514709640beceead7aee45afa406e7dc25f
Zhannur <jannuraidynuly@gmail.com>
2022-04-13 12:52:21 +0500

    endpoints

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
-import Post from "./Post.js";
+import router from "./router.js";
 
 const DB_URL = `mongodb+srv://police:Police12@cluster0.nnice.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 const PORT = 5001;
 const app = express();
+
 app.use(express.json())
-
-app.get('/', (req, res) => {
-    console.log(req.query.third);
-    res.status(200).json('Server job')
-});
-
-app.post('/', async (req, res) => {
-    try {
-        const { author, title, content, picture } = req.body
-        const post = await Post.create({author, title, content, picture})
-        res.json(post)
-    } catch (e) {
-        res.status(500).json(e)
-    }
-})
+app.use('/api', router)
 
 async function startApp(){
     try {
